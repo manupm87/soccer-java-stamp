ca34eb75c30c50c72d5c70bfe1bcd7980f7d4dc0
Manuel Perez <manuel.p.martinez@nokia-bell-labs.com>
2017-11-16 15:29:28 +0100

    Add support for JUnit tests

---
+++ src/test/java/com/nokia/e2eo/SoccerPlayerTests.java
@@ -0,0 +1,14 @@
+package com.nokia.e2eo;
+
+import static org.junit.Assert.fail;
+
+import org.junit.Test;
+
+class SoccerPlayerTests {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
